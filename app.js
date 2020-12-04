a519afeca4f1f7ad76e4b13d3f0b3c51d4cf2996
Arielle M <69764349+DoctorStupidMD@users.noreply.github.com>
2020-12-04 19:34:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,263 @@
+/*global jQuery, Handlebars, Router */
+// jQuery(function ($) {
+
+  'use strict';
+
+  Handlebars.registerHelper('eq', function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this);
+  });
+  
+  var ENTER_KEY = 13;
+  var ESCAPE_KEY = 27;
+  
+  var util = {
+    uuid: function () {
+      /*jshint bitwise:false */
+      var i, random;
+      var uuid = '';
+  
+      for (i = 0; i < 32; i++) {
+        random = Math.random() * 16 | 0;
+        if (i === 8 || i === 12 || i === 16 || i === 20) {
+          uuid += '-';
+        }
+        uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+      }
+  
+      return uuid;
+    },
+    pluralize: function (count, word) {
+      return count === 1 ? word : word + 's';
+    },
+    store: function (namespace, data) {
+      if (arguments.length > 1) {
+        return localStorage.setItem(namespace, JSON.stringify(data));
+      } else {
+        var store = localStorage.getItem(namespace);
+        return (store && JSON.parse(store)) || [];
+      }
+    }
+  };
+  
+  var App = {
+    init: function () {
+      this.todos = util.store('todos-jquery');
+      // this.todoTemplate = Handlebars.compile($('#todo-template').html());
+      this.todoTemplate = Handlebars.compile(document.getElementById('todo-template').innerHTML);
+      // this.footerTemplate = Handlebars.compile($('#footer-template').html());
+      this.footerTemplate = Handlebars.compile(document.getElementById('footer-template').innerHTML);
+      this.bindEvents();
+  
+      new Router({
+        '/:filter': function (filter) {
+          this.filter = filter;
+          this.render();
+        }.bind(this)
+      }).init('/all');      
+    },
+    bindEvents: function () {
+      // $('#new-todo').on('keyup', this.create.bind(this));
+      document.getElementById('new-todo').addEventListener('keyup', this.create.bind(this));
+      // $('#toggle-all').on('change', this.toggleAll.bind(this));
+      document.getElementById('toggle-all').addEventListener('change', this.toggleAll.bind(this));
+      // $('#footer').on('click', '#clear-completed', this.destroyCompleted.bind(this));
+      document.getElementById('footer').addEventListener('click', function(e) {
+        if (e.target.id === 'clear-completed') {
+          this.destroyCompleted();
+        }
+      }.bind(this));
+      
+      // $('#todo-list')
+      //   .on('change', '.toggle', this.toggle.bind(this))
+      //   .on('dblclick', 'label', this.edit.bind(this))
+      //   .on('keyup', '.edit', this.editKeyup.bind(this))
+      //   .on('focusout', '.edit', this.update.bind(this))
+      //   .on('click', '.destroy', this.destroy.bind(this));
+    
+      document.getElementById('todo-list').addEventListener('change', function(e) {
+        if (e.target.className === 'toggle') {
+          this.toggle(e);
+        }
+      }.bind(this));
+      document.getElementById('todo-list').addEventListener('dblclick', function (e) {
+        if (e.target.localName === 'label') {
+          this.edit(e);
+        }
+      }.bind(this));
+      document.getElementById('todo-list').addEventListener('keyup', function (e) {
+        if (e.target.className === 'edit') {
+          this.editKeyup(e);
+        }
+      }.bind(this));
+      document.getElementById('todo-list').addEventListener('focusout', function (e) {
+        if (e.target.className === 'edit') {
+          this.update(e);
+        }
+      }.bind(this));
+      document.getElementById('todo-list').addEventListener('click', function (e) {
+        if (e.target.className === 'destroy') {
+          this.destroy(e);
+        }
+      }.bind(this));
+    },
+    
+    render: function () {
+      var todos = this.getFilteredTodos();
+      // $('#todo-list').html(this.todoTemplate(todos));
+      document.getElementById('todo-list').innerHTML = this.todoTemplate(todos);
+      // $('#main').toggle(todos.length > 0);
+      document.getElementById('main').style.display = todos.length > 0 ? 'block' : 'none';
+      // $('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
+      document.getElementById('toggle-all').checked = this.getActiveTodos().length === 0;
+      this.renderFooter();
+      // $('#new-todo').focus();
+      document.getElementById('new-todo').focus();
+      util.store('todos-jquery', this.todos);
+    },
+    renderFooter: function () {
+      var todoCount = this.todos.length;
+      var activeTodoCount = this.getActiveTodos().length;
+      var template = this.footerTemplate({
+        activeTodoCount: activeTodoCount,
+        activeTodoWord: util.pluralize(activeTodoCount, 'item'),
+        completedTodos: todoCount - activeTodoCount,
+        filter: this.filter
+      });
+  
+      // $('#footer').toggle(todoCount > 0).html(template);
+      var footer = document.getElementById('footer');
+      if (todoCount > 0) {
+        footer.style.display = 'block';
+        footer.innerHTML = template;
+      } else {
+        footer.style.display = 'none';
+      }
+    },
+    toggleAll: function (e) {
+      // var isChecked = $(e.target).prop('checked');
+      var isChecked = e.target.checked;
+  
+      this.todos.forEach(function (todo) {
+        todo.completed = isChecked;
+      });
+  
+      this.render();
+    },
+    getActiveTodos: function () {
+      return this.todos.filter(function (todo) {
+        return !todo.completed;
+      });
+    },
+    getCompletedTodos: function () {
+      return this.todos.filter(function (todo) {
+        return todo.completed;
+      });
+    },
+    getFilteredTodos: function () {
+      if (this.filter === 'active') {
+        return this.getActiveTodos();
+      }
+  
+      if (this.filter === 'completed') {
+        return this.getCompletedTodos();
+      }
+  
+      return this.todos;
+    },
+    destroyCompleted: function () {
+      this.todos = this.getActiveTodos();
+      this.filter = 'all';
+      this.render();
+    },
+    // accepts an element from inside the `.item` div and
+    // returns the corresponding index in the `todos` array
+    indexFromEl: function (el) {
+      // var id = $(el).closest('li').data('id');
+      var id = el.closest('li').dataset.id;
+      var todos = this.todos;
+      var i = todos.length;
+  
+      while (i--) {
+        if (todos[i].id === id) {
+          return i;
+        }
+      }
+    },
+    create: function (e) {
+      // var $input = $(e.target);
+      var input = e.target;
+      // var val = $input.val().trim();
+      var val = input.value.trim();
+  
+      if (e.which !== ENTER_KEY || !val) {
+        return;
+      }
+  
+      this.todos.push({
+        id: util.uuid(),
+        title: val,
+        completed: false
+      });
+  
+      // $input.val('');
+      input.value = '';
+  
+      this.render();
+    },
+    toggle: function (e) {
+      var i = this.indexFromEl(e.target);
+      this.todos[i].completed = !this.todos[i].completed;
+      this.render();
+    },
+    edit: function (e) {
+      // var $input = $(e.target).closest('li').addClass('editing').find('.edit');
+      var el = e.target.closest('li');
+      el.classList.add('editing');
+      var input = el.querySelector('.edit');
+      // $input.val($input.val()).focus();
+      input.focus(); 
+      // not required, but I prefer the cursor to flush right in the editing field
+      var temp = input.value;
+      input.value = '';
+      input.value = temp;
+    },
+    editKeyup: function (e) {
+      if (e.which === ENTER_KEY) {
+        e.target.blur();
+      }
+  
+      if (e.which === ESCAPE_KEY) {
+        // $(e.target).data('abort', true).blur();
+        e.target.dataset.abort = true;
+        e.target.blur();
+      }
+    },
+    update: function (e) {
+      var el = e.target;
+      // var $el = $(el);
+      // var val = $el.val().trim();
+      var val = el.value.trim();
+  
+      if (!val) {
+        this.destroy(e);
+        return;
+      }
+  
+      // if ($el.data('abort')) {
+      if (el.dataset.abort) {
+        // $el.data('abort', false);
+        el.dataset.abort = false;
+      } else {
+        this.todos[this.indexFromEl(el)].title = val;
+      }
+  
+      this.render();
+    },
+    destroy: function (e) {
+      this.todos.splice(this.indexFromEl(e.target), 1);
+      this.render();
+    }
+  };
+  
+  App.init();
+  // });
---
+++ app_spec.js
@@ -0,0 +1,135 @@
+function resetList() {
+  var todoLis = document.querySelectorAll('#todo-list li');
+  var deleteButtons = document.getElementsByClassName('destroy');
+  for (var i = 0; i < todoLis.length; i++) {
+    deleteButtons[0].click();
+  }
+}
+
+function createTodo() {
+  var input = document.getElementById('new-todo');
+  input.value = 'Frameworks suuuuuck';
+  pressEnterKey(input);
+  return document.querySelector('li');
+}
+
+function pressEnterKey(input) {
+  var enterKeyEvent = document.createEvent('Event');
+  enterKeyEvent.initEvent('keyup');
+  enterKeyEvent.keyCode = 13;
+  input.dispatchEvent(enterKeyEvent);
+}
+
+function enableEditing(todoLabel) {
+  var doubleClickEvent = new MouseEvent('dblclick', {
+    bubbles: true,
+    cancelable: true,
+    view: window
+  });
+  todoLabel.dispatchEvent(doubleClickEvent);
+}
+
+function clickToggleAllButton() {
+  var toggleAllButton = document.getElementById('toggle-all');
+  toggleAllButton.click();
+}
+
+tests({
+  'It should add a todo.': function() {
+    resetList();
+    createTodo();
+    var todoLis = document.querySelectorAll('#todo-list li');
+    eq(todoLis.length, 1);
+  },
+
+  'It should delete a todo.': function() {
+    resetList();
+    createTodo();
+    var deleteButtons = document.getElementsByClassName('destroy');
+    deleteButtons[0].click();
+    eq(deleteButtons.length, 0);
+  },
+
+  'It should toggle a todo.': function() {
+    resetList();
+    createTodo();
+    var toggleButtons = document.getElementsByClassName('toggle');
+    toggleButtons[0].click();
+    eq(toggleButtons[0].checked, true);
+
+    clickToggleAllButton();
+  },
+
+  'It should toggle all todos.': function() {
+    resetList();
+    createTodo();
+    createTodo();
+    var toggleButtons = document.getElementsByClassName('toggle');
+    toggleButtons[0].click();
+
+    clickToggleAllButton();
+    
+    eq(toggleButtons[0].checked, true);
+    eq(toggleButtons[1].checked, true);
+  },
+
+  'It should edit a todo.': function() {
+    resetList();
+    var todo = createTodo();
+    var todoInput = todo.querySelector('input.edit');
+    var todoLabel = todo.querySelector('label');
+    var todoTitleEdited = 'Edited';
+
+    enableEditing(todoLabel);
+    todoInput.dispatchEvent(new Event('focus'));
+    todoInput.value = todoTitleEdited;
+    todoInput.dispatchEvent(new Event('blur'));
+
+    var todoLabelAfterRender = document.querySelector('#todo-list label');
+    eq(todoLabelAfterRender.textContent, todoTitleEdited);
+  },
+
+  // 'It should not contain a Handlebars script tag.': function() {
+  //   var nodeListScripts = document.querySelectorAll('script');
+  //   var handlebarsScriptTags = [];
+  //   function findScriptTags() {
+  //     for (var i = 0; i < nodeListScripts.length; i++) {
+  //       if (nodeListScripts[i].outerHTML === '<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.js"></script>') {
+  //         handlebarsScriptTags.push(nodeListScripts[i]);
+  //       }
+  //     }
+  //   }
+  //   findScriptTags();
+  //   eq(handlebarsScriptTags.length, 0);
+  // },
+
+  'It should not contain a jQuery script tag.': function() {
+    var nodeListScripts = document.querySelectorAll('script');
+    var jqueryScriptTags = [];
+    function findScriptTags() {
+      for (var i = 0; i < nodeListScripts.length; i++) {
+        if (nodeListScripts[i].outerHTML === '<script src="https://code.jquery.com/jquery-2.2.4.js"></script>') {
+          jqueryScriptTags.push(nodeListScripts[i]);
+        }
+      }
+    }
+    findScriptTags();
+    eq(jqueryScriptTags.length, 0);
+  },
+
+  'It should contain the app.js tag.': function() {
+    var nodeListScripts = document.querySelectorAll('script');
+    var appjsScriptTags = [];
+    function findScriptTags() {
+      for (var i = 0; i < nodeListScripts.length; i++) {
+        if (nodeListScripts[i].outerHTML === '<script src="app.js"></script>') {
+          appjsScriptTags.push(nodeListScripts[i]);
+        }
+      }
+    }
+    findScriptTags();
+    eq(appjsScriptTags.length, 1);
+    eq(JSON.stringify(appjsScriptTags[0].outerHTML), JSON.stringify('<script src="app.js"></script>'));
+  }
+});
+
